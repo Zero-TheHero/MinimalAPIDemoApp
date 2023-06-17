afe746f88bcb51b8fe8f936526a1ee61582dfcd7
TheDude <cassinelli@themoon.co.uk>
2023-06-17 21:45:29 +0100

    Unused using's tidied up

--- DataAccess/Data/UserData.cs
+++ DataAccess/Data/UserData.cs
@@ -1,10 +1,5 @@
 ﻿using DataAccess.DbAccess;
 using DataAccess.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Data;
 
