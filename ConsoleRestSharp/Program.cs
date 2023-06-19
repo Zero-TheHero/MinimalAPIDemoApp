68c950785e3630ccd17166d8b16c24022676edd4
TheDude <cassinelli@themoon.co.uk>
2023-06-19 18:45:55 +0100

    Select single user

--- ConsoleRestSharp/Program.cs
+++ ConsoleRestSharp/Program.cs
@@ -13,10 +13,29 @@ var response = client.Execute(request);
 if (response.IsSuccessful && (response.Content != null) )
 {
     var users = JsonConvert.DeserializeObject<List<UserModel>>(response.Content.ToString());
-
     if (users != null)
-    foreach (var user in users)
     {
+        Console.WriteLine("List: ");
+        foreach (var user in users)
+        {
+            Console.WriteLine("{0} {1} {2}", user.Id, user.FirstName, user.LastName);
+        }
+    }
+}
+
+request = new RestRequest("Users/3", Method.Get)
+{
+    RequestFormat = DataFormat.Json
+};
+
+response = client.Execute(request);
+
+if (response.IsSuccessful && (response.Content != null))
+{
+    var user = JsonConvert.DeserializeObject<UserModel>(response.Content.ToString());
+    if (user != null)
+    {
+        Console.WriteLine("Single: ");
         Console.WriteLine("{0} {1} {2}", user.Id, user.FirstName, user.LastName);
     }
 }
