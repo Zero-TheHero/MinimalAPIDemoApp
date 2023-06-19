eff8f38894815196c531908a1b47aee6166f1151
TheDude <cassinelli@themoon.co.uk>
2023-06-19 12:45:52 +0100

    Console project added

---
+++ ConsoleRestSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Models;
+using Newtonsoft.Json;
+using RestSharp;
+
+var client = new RestClient("https://localhost:5001");
+var request = new RestRequest("Users", Method.Get)
+{
+    RequestFormat = DataFormat.Json
+};
+var response = client.Execute(request);
+
+if (response.Content != null)
+{
+    var users = JsonConvert.DeserializeObject<List<UserModel>>(response.Content.ToString());
+
+    if (users != null)
+    foreach (var user in users)
+    {
+        Console.WriteLine("{0} {1} {2}", user.Id, user.FirstName, user.LastName);
+    }
+}
+
+
+Console.ReadLine();
+
+
--- MinimalAPIDemo/Program.cs
+++ MinimalAPIDemo/Program.cs
@@ -3,16 +3,12 @@ using MinimalAPIDemo;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddSingleton<IUserData, UserData>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseSwagger();
 app.UseSwaggerUI();
