30ae393b553f4ef038ace97b439229b55a65725f
TheDude <cassinelli@themoon.co.uk>
2023-06-28 16:17:22 +0100

    Xunit tests with IClassFixture

--- MinimalApiTest/MinimalApiTests.cs
+++ MinimalApiTest/MinimalApiTests.cs
@@ -3,15 +3,38 @@ using System.Net;
 
 namespace MinimalApiDemo.Test;
 
-public class MinimalApiTests
+public class MinimalApiTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public async void IsApiWorking()
+    private readonly HttpClient _client;
+    private HttpResponseMessage? _response;
+
+    public MinimalApiTests(WebApplicationFactory<Program> factory)
     {
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
-        var response = await client.GetAsync("/Api/GetTests");
-        var data = await response.Content.ReadAsStringAsync();
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async void GetUsers()
+    {
+        //await using var application = new WebApplicationFactory<Program>();
+        //using var client = application.CreateClient();
+        _response = await _client.GetAsync("/Api/GetTests");
+        //var data = await response.Content.ReadAsStringAsync();
+        Assert.Equal(HttpStatusCode.OK, _response.StatusCode);
+        
+    }
+
+    [Fact]
+    public async void GetUser()
+    {
+        _response = await _client.GetAsync("/Api/GetTest/1");
+        Assert.Equal(HttpStatusCode.OK, _response.StatusCode);
+    }
+
+    [Fact]
+    public async void DeleteUser()
+    {
+        _response = await _client.DeleteAsync("/Api/DeleteTest/2");
+        Assert.Equal(HttpStatusCode.OK, _response.StatusCode);
     }
 }
