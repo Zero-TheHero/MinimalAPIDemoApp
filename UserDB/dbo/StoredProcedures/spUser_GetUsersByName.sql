93ba86d2d9c8f7f7b5b3991abc0db650a1619e57
TheDude <cassinelli@themoon.co.uk>
2023-07-14 11:08:10 +0100

    Allow null for Get Users By Name

--- UserDB/dbo/StoredProcedures/spUser_GetUsersByName.sql
+++ UserDB/dbo/StoredProcedures/spUser_GetUsersByName.sql
@@ -1,8 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[spUser_GetUsersByName]
-	@name varchar(50)
+	@name varchar(50) = ''
 AS
 BEGIN
 	SELECT Id, FirstName, LastName 
 	FROM dbo.[User]
-	Where Firstname like @name or LastName Like @Name
+	Where Firstname like @name or LastName Like @Name or @name =''
 END
